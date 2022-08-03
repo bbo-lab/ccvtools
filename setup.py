be59e00d9f9d506333abfba3fa1b5a4d6a8a3be9
Kay-Michael Voit <kay-michael.voit@caesar.de>
2022-08-03 12:02:45 +0200

    v1.2.0 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="bbo-ccvtools",
-    version="1.1.6",
+    version="1.2.0",
     description="Work with BBO CCV files",
     long_description=README,
     long_description_content_type="text/markdown",
